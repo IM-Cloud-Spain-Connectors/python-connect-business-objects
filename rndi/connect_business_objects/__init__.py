9de8263deaa1c38eee4bf115933efb963147134b
Unay Santisteban <davidunay.santisteban@cloudblue.com>
2023-01-09 16:03:57 +0100

    Feature/Updated Copyright Year.

--- rndi/connect_business_objects/__init__.py
+++ rndi/connect_business_objects/__init__.py
@@ -1,5 +1,5 @@
 #
 # This file is part of the Ingram Micro CloudBlue Connect Processors Toolkit.
 #
-# Copyright (c) 2022 Ingram Micro. All Rights Reserved.
+# Copyright (c) 2023 Ingram Micro. All Rights Reserved.
 #
